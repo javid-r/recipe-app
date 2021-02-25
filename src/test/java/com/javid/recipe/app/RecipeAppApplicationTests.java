5180ead6f8925aa460afe92a4caf4140f27c27de
javaj-r <javid.rafiee@gmail.com>
2021-02-25 23:26:48 +0330

    Initialize application. Closes #1.

---
+++ src/test/java/com/javid/recipe/app/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javid.recipe.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
